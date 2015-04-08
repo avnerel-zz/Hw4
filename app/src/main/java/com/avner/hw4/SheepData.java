b911a1c1b6a85ec5d6a919e9010428591f2502b7
Avner Elizarov <avnerelizarov@gmail.com>
2015-04-08 14:48:45 +0300

    v1

---
+++ app/src/main/java/com/avner/hw4/SheepData.java
@@ -0,0 +1,24 @@
+package com.avner.hw4;
+
+/**
+ * Created by avner on 07/04/2015.
+ */
+public class SheepData {
+
+    public String name;
+
+    public String age;
+
+    public String color;
+
+    public SheepData(String name, String age, String color){
+
+        this.name = name;
+
+        this.age = age;
+
+        this.color = color;
+    }
+}
+
+
